783b09829ed4721a19f87beb9652f4e96ad3c7bf
Pattaraporn Bodhisuwan <mp@dgm59.com>
2010-12-11 01:01:20 +0700

    Added Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackline::Application.config.secret_token = 'e804e19aa31ed954da7e2312ac976b9c9aebbed9ac8be1c7ee10de9af139960b055f3eedc931a52f7aa88a08191890cc7a5f6905842630af8324aa908b04b8bf'
