28b81bb7a7558e5ca9ecbc700af97ece5f9f7f82
Pattaraporn Bodhisuwan <mp@dgm59.com>
2010-12-11 03:49:46 +0700

    Updated gitignore and added active and inactive routes in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,29 @@
 class ProjectsController < InheritedResources::Base
   before_filter :authenticate_user!
+  
+  def active
+    resource.active = true
+    resource.save
+    redirect_to collection_url, :notice => "Task: #{resource.body} is done!"
+  end
+  
+  def inactive
+    resource.active = false
+    resource.save
+    redirect_to collection_url, :notice => "Task: #{resource.body} is undone!"
+  end
+  
+  def done
+    resource.done = true
+    resource.completed_on = DateTime.now
+    resource.save
+    redirect_to collection_url, :notice => "Task: #{resource.body} is done!"
+  end
+  
+  def undone
+    resource.done = false
+    resource.completed_on = nil
+    resource.save
+    redirect_to collection_url, :notice => "Task: #{resource.body} is undone!"
+  end
 end
